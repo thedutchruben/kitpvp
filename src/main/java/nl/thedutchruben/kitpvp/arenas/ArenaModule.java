ed0fa022c7bbb602aaa32e59cd8cfaea87307db1
Ruben de Roos <deroosr@gmail.com>
2020-04-28 22:08:09 +0200

    Arena module
    + add basic setup of the arenas

--- src/main/java/nl/thedutchruben/kitpvp/arenas/ArenaModule.java
+++ src/main/java/nl/thedutchruben/kitpvp/arenas/ArenaModule.java
@@ -1,14 +1,31 @@
 package nl.thedutchruben.kitpvp.arenas;
 
+import nl.thedutchruben.kitpvp.KitPvp;
+import nl.thedutchruben.kitpvp.arenas.objects.Arena;
 import nl.thedutchruben.kitpvp.registery.Module;
 
+import java.util.List;
+
+/**
+ * Arena module to handle the arenas
+ */
 public class ArenaModule extends Module {
+    private List<Arena> arenas;
 
     public void load() {
-
+        //Load the arenas from the database
+        arenas = KitPvp.getInstance().getStorageModule().getStorage().loadArenas();
     }
 
     public void unLoad() {
+        arenas.clear();
+    }
 
+    /**
+     * Get a list with the cashed arena's
+     * @return A list with arena's
+     */
+    public List<Arena> getArenas() {
+        return arenas;
     }
 }

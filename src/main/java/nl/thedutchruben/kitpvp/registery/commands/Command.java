f40d370de1d8c95dc90b24255e41ed478d1e6cdf
Ruben de Roos <deroosr@gmail.com>
2020-04-28 23:04:29 +0200

    Documentation
    + added documentation to the command

--- src/main/java/nl/thedutchruben/kitpvp/registery/commands/Command.java
+++ src/main/java/nl/thedutchruben/kitpvp/registery/commands/Command.java
@@ -11,24 +11,45 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * An abstract class witch can be implemented to create commands for this project
+ *
+ * @author Ruben
+ * @since 1.0-SNAPSHOT
+ */
 public abstract class Command extends org.bukkit.command.Command implements TabCompleter {
-
     private final String commandPermissionPrefix = "kitpvp.command.";
 
     private List<SubCommand> subCommands;
-
     private boolean defaultList = true;
 
+    /**
+     * Default creating of the command
+     * @param command The Command name
+     * @param permission The name of the permission that wil be added after 'kitpvp.command.'
+     */
     public Command(String command, String permission) {
         super(command);
         setPermission(commandPermissionPrefix + permission);
         this.subCommands = new ArrayList<>();
     }
 
+    /**
+     * Add a subcommand to the command
+     * @param subCommand the sub command to be added
+     */
     public void addSubCommand(SubCommand subCommand) {
         subCommands.add(subCommand);
     }
 
+    /**
+     * Executes the command, returning its success
+     *
+     * @param sender Source object which is executing this command
+     * @param commandLabel The alias of the command used
+     * @param args All arguments passed to the command, split via ' '
+     * @return true if the command was successful, otherwise false
+     */
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
         if (args.length <= 0) {
@@ -72,6 +93,11 @@ public abstract class Command extends org.bukkit.command.Command implements TabC
         return false;
     }
 
+    /**
+     * Send info about a sub command
+     * @param sender The command sender
+     * @param command One of the sub commands
+     */
     public void sendInfo(CommandSender sender, SubCommand command){
         if(command.getSubCommands().isEmpty())return;
         for(SubCommand subCommand : command.getSubCommands()){
@@ -82,13 +108,33 @@ public abstract class Command extends org.bukkit.command.Command implements TabC
         }
     }
 
+    /**
+     * Set if the command send the default list
+     * @param defaultList
+     */
     public void setDefaultList(boolean defaultList) {
         this.defaultList = defaultList;
     }
 
+    /**
+     * If the defaultList is false then the command wil execute the executeDefault method
+     * @param commandSender the command sender of the command
+     * @param args the args that is given by the commandsender
+     */
     public void executeDefault(CommandSender commandSender, String[] args){
     }
 
+    /**
+     * Executed on tab completion for this command, returning a list of
+     * options the player can tab through.
+     *
+     * @param sender Source object which is executing this command
+     * @param alias the alias being used
+     * @param args All arguments passed to the command, split via ' '
+     * @return a list of tab-completions for the specified arguments. This
+     *     will never be null. List may be immutable.
+     * @throws IllegalArgumentException if sender, alias, or args is null
+     */
     @Override
     public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String alias, String[] args) {
         List<String> commands = new ArrayList<>();

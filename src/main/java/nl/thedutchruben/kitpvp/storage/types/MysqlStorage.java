2bb9b6c38243af20d59145a4772c59e2abc8a436
Ruben de Roos <deroosr@gmail.com>
2020-04-28 22:51:24 +0200

    Documentation update
    + Update the documentation to all the type classes

--- src/main/java/nl/thedutchruben/kitpvp/storage/types/MysqlStorage.java
+++ src/main/java/nl/thedutchruben/kitpvp/storage/types/MysqlStorage.java
@@ -11,56 +11,86 @@ import java.util.UUID;
 public class MysqlStorage extends Storage {
 
     /**
-     * Load the player from uuid if the player doesn't exist they wil create the player
+     * Load a {@link KitPvpPlayer} from a {@link UUID}, if the player doesn't exist when fetching
+     * from the storage the called method will generate a {@link KitPvpPlayer} with default values.
+     * This may be different for every implementation on how exactly this is handled.
      *
-     * @param uuid The UUID of the player
-     * @return The kitPvpPlayer of the loaded player
+     * @param uuid The {@link UUID} of the in-game player to retrieve the data for
+     * @return a {@link KitPvpPlayer} with either default values or values retrieved from
+     * the storage implementation
+     * @see KitPvpPlayer
+     * @see UUID
+     * @since 1.0-SNAPSHOT
      */
+    @Override
     public KitPvpPlayer loadPlayer(UUID uuid) {
         return null;
     }
 
     /**
-     * Save the player in the database
+     * Saves all the given {@link KitPvpPlayer} it's internal data to the storage,
+     * such data may be player names, in-game kills / deaths, in-game bought kits etc.
+     * This may be different for every implementation on how exactly this is handled.
      *
-     * @param kitPvpPlayer the player that needs to be saved
+     * @param kitPvpPlayer the object that will be saved
+     * @see KitPvpPlayer
+     * @since 1.0-SNAPSHOT
      */
+    @Override
     public void savePlayer(KitPvpPlayer kitPvpPlayer) {
 
     }
 
     /**
-     * Load all the arena's from the database
+     * Loads all the {@link Arena} objects from the storage, the implementation should make
+     * sure to load all of them. This may be different for every implementation on how exactly this is handled.
      *
-     * @return A list of all the arenas that are stored in the database
+     * @return a {@link List<Arena>} with all found objects in the storage
+     * @see Arena
+     * @since 1.0-SNAPSHOT
      */
+    @Override
     public List<Arena> loadArenas() {
         return null;
     }
 
     /**
-     * Save the arena in the database
+     * Saves the given {@link Arena} object to the storage.
+     * This may be different for every implementation on how exactly this is handled.
      *
-     * @param arena the arena that needs to be saved
+     * @param arena the object to save
+     * @see Arena
+     * @since 1.0-SNAPSHOT
      */
+    @Override
     public void saveArena(Arena arena) {
 
     }
 
     /**
-     * Load the kits from the database
+     * Loads all {@link Kit} objects from the storage.
+     * This may be different for every implementation on how exactly this is handled.
      *
-     * @return A list with all the kits that are stored in the database
+     * @return a {@link List<Kit>} with all the {@link Kit} objects found by the
+     * storage implementation
+     * @see Kit
+     * @since 1.0-SNAPSHOT
      */
+    @Override
     public List<Kit> loadKits() {
         return null;
     }
 
     /**
-     * Save the kit in the database
+     * Saves the given {@link Kit} to the storage, this will make sure that {@link Storage#loadKits()} will
+     * load the given {@link Kit} after a restart of the application.
+     * This may be different for every implementation on how exactly this is handled.
      *
-     * @param kit The kit that needs to be saved
+     * @param kit the {@link Kit} to save to the storage
+     * @see Kit
+     * @since 1.0-SNAPSHOT
      */
+    @Override
     public void saveKit(Kit kit) {
 
     }
